8bdeceeb124fc67b25b16a9fc3e458d159120958
Henry Bauer <github@henrybauer.com>
2017-10-23 20:47:17 +0300

    Recompiled for KSP 1.3.1. Removed some unused variables, and prepared for SSTU compatibility (not working yet)

--- SafeChute.cs
+++ SafeChute.cs
@@ -11,9 +11,7 @@ namespace GenesisRage
 		public object partModule;
 		public bool dewarpedAtDeploy = false;
 		public bool dewarpedAtGround = false;
-		public bool isRealChute = false;
-		public bool isFARChute = false;
-
+		
 		public SafeChutePart(PartModule pm)
 		{
 			partModule = pm;
@@ -25,7 +23,20 @@ namespace GenesisRage
 		public abstract bool isDeployed();
 	}
 
-	public class SafeChuteFARPart : SafeChutePart
+/*    public class SafeChuteSSTUPart : SafeChutePart
+    {
+        public override bool isDeployed()
+        {
+            return false;
+        }
+
+        public SafeChuteSSTUPart(PartModule pm) : base(pm)
+        { }
+
+    }*/
+
+
+    public class SafeChuteFARPart : SafeChutePart
 	{
 		public override bool isDeployed()
 		{
@@ -116,10 +127,11 @@ namespace GenesisRage
 #endif
 					switch (vessel.parts[i].Modules[j].moduleName)
 					{
-						case "ModuleParachute": safeParts.Add(new SafeChuteStockPart(vessel.parts[i].Modules[j])); break;
-						case "RealChuteModule": safeParts.Add(new SafeChuteRCPart   (vessel.parts[i].Modules[j])); break;
-						case "RealChuteFAR":    safeParts.Add(new SafeChuteFARPart  (vessel.parts[i].Modules[j])); break;
-					}
+						case "ModuleParachute":      safeParts.Add(new SafeChuteStockPart(vessel.parts[i].Modules[j])); break;
+						case "RealChuteModule":      safeParts.Add(new SafeChuteRCPart   (vessel.parts[i].Modules[j])); break;
+						case "RealChuteFAR":         safeParts.Add(new SafeChuteFARPart  (vessel.parts[i].Modules[j])); break;
+                        //case "SSTUModularParachute": safeParts.Add(new SafeChuteSSTUPart (vessel.parts[i].Modules[j])); break;
+                    }
 				}
 			}
 		}
