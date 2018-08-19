0a5074d4d589a2d348d71e1ce501d7b129d6cbf9
Henry Bauer <github@henrybauer.com>
2018-08-19 09:51:52 +0300

    Bump version to 2.1.14 for KSP 1.4.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.13")]
-[assembly: AssemblyFileVersion("2.1.13")]
+[assembly: AssemblyVersion("2.1.14")]
+[assembly: AssemblyFileVersion("2.1.14")]
