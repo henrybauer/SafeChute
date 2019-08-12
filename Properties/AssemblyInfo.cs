54887e2490f87ff25790fbb17bd0b98f53372252
Henry Bauer <github@henrybauer.com>
2019-08-11 20:29:05 -0500

    Fix .NET managed references

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.17")]
-[assembly: AssemblyFileVersion("2.1.17")]
+[assembly: AssemblyVersion("2.1.18")]
+[assembly: AssemblyFileVersion("2.1.18")]
