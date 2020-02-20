d0d194088717a82ea31ab6522af53b9e9f42a5d6
Henry Bauer <github@henrybauer.com>
2020-02-20 10:42:24 -0600

    Update for KSP 1.9

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.18")]
-[assembly: AssemblyFileVersion("2.1.18")]
+[assembly: AssemblyVersion("2.1.19")]
+[assembly: AssemblyFileVersion("2.1.19")]
